28eba21376e2b5fd7527b39d06fc8dfdd3c86d0b
rahidhaque <ontirahid@yahoo.com>
2022-05-25 15:06:07 +0600

    add admin functionality added

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
+const UserRow = ({ user, refetch }) => {
     const { email, role } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    toast.warning('You are forbidden to make someone an admin!');
+                }
+                return res.json();
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success('Admin Added Successfully');
+                }
+            })
+    }
     return (
         <tr>
             <td>{email}</td>
-            <td>{role === 'admin' ? <button class="btn btn-xs btn-success" disabled="disabled">Already Admin</button> : <button className="btn btn-xs"><span className='px-2'>Make Admin</span><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg></button>}</td>
+            <td>{role === 'admin' ? <button class="btn btn-xs btn-success" disabled="disabled">Already Admin</button> : <button onClick={makeAdmin} className="btn btn-xs"><span className='px-2'>Make Admin</span><svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg></button>}</td>
         </tr >
     );
 };
